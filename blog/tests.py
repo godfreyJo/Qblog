1276c6802e77fb989268f613b043e47c6fb525bd
godfreyJo <godfreyoyugi@gmail.com>
2019-03-07 02:04:41 +0300

    modification of the tests

--- blog/tests.py
+++ blog/tests.py
@@ -7,17 +7,18 @@ from .models import Post
 
 
 class BlogTests(TestCase):
+
 	def setUp(self):
 		self.user = get_user_model().objects.create_user(
 		username='testuser',
 		email='test@email.com',
-		password='secret'
-)
-	self.post = Post.objects.create(
+		password='secret')
+
+		self.post = Post.objects.create(
 		title='A good title',
 		body='Nice body content',
-		author=self.user,
-)	
+		author=self.user)	
+
 	def test_string_representation(self):
 		post = Post(title='A sample title')
 		self.assertEqual(str(post), post.title)
@@ -26,11 +27,13 @@ class BlogTests(TestCase):
 		self.assertEqual(f'{self.post.title}', 'A good title')
 		self.assertEqual(f'{self.post.author}', 'testuser')
 		self.assertEqual(f'{self.post.body}', 'Nice body content')
+
 	def test_post_list_view(self):
 		response = self.client.get(reverse('home'))
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, 'Nice body content')
 		self.assertTemplateUsed(response, 'home.html')
+
 	def test_post_detail_view(self):
 		response = self.client.get('/post/1/')
 		no_response = self.client.get('/post/100000/')
--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 
 path('', views.BlogListView.as_view(), name='home'),
 path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
 
 ]
